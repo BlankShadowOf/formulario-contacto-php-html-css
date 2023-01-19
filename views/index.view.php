1498b4c29c7b1d5cef21af4766d2df869ccf1420
Blank Shadow <21046642+BlankShadowOf@users.noreply.github.com>
2023-01-18 21:18:19 -0500

    Formulario de contacto v1.0
    Primera version definitiva 1.0 del formulario de contacto.

---
+++ views/index.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Formulario de contacto</title>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="wrap">
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+			<h1>Contacto</h1>
+			<input type="text" class="form-control" name="nombre" placeholder="Nombre:" value="<?php if(!$enviado && isset($nombre)) echo $nombre; ?>" id="nombre">
+			<input type="text" class="form-control" name="correo" placeholder="Correo:" value="<?php if(!$enviado && isset($correo)) echo $correo; ?>" id="correo">
+			<textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje:"><?php if(!$enviado && isset($mensaje)) echo $mensaje; ?></textarea>
+
+			<?php if (!empty($errores)): ?>
+				<div class="alert error">
+					<?php echo $errores; ?>
+				</div>
+			<?php elseif($enviado): ?>
+				<div class="alert success">
+					<p>Se envio con exito.</p>
+				</div>
+			<?php endif ?>
+
+			<input type="submit" name="submit" class="btn btn-primary" value="Enviar correo">
+		</form>
+	</div>
+</body>
+</html>
